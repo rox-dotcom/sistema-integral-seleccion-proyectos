6a9301d771ba61554ecaa64a4fd11cc30a7ef9ed
rox-dotcom <roxarandagr@gmail.com>
2025-02-26 11:06:12 -0600

    added style and validation

--- frontend/src/login.js
+++ frontend/src/login.js
@@ -6,43 +6,50 @@ import { redirectTo } from "./modules/user_fetch.js"
 const IDs = {
     loginUsername: "login-user-name",
     loginButton: "login",
-
-    createUsername: "create-user-name",
-    createUserButton: "create-user",
+    signinButton: "signin",
+    passwordUser: "password-user",
 
     deleteUsername: "delete-user-name",
     deleteUserButton: "delete-user",
 };
+
 
 document.addEventListener("DOMContentLoaded", _ => {
     const elems = Object.keys(IDs).reduce((output, id) => {
         output[IDs[id]] = document.getElementById(IDs[id]);
         return output;
     }, {});
+    
 
+    //checking if user exists, and returning (app or alert)
     elems[IDs.loginButton].addEventListener("click", _ => {
         const username = elems[IDs.loginUsername].value;
+        const password= elems[IDs.passwordUser].value;
+
+        if(!username||!password){
+            alert("Por favor llena todos los campos");
+            return;
+        }
+        
+
         const request = backend.existsUser(username);
         requestFeedback(request, elems[IDs.loginButton], "", "Error");
         request.then(exists => {
             if (!exists) {
-                alert("Usuario no encontrado");
+                alert("Usuario o contraseña incorrectos");
                 return;
             }
             redirectTo("app", username);
         });
     });
 
-    elems[IDs.createUserButton].addEventListener("click", _ => {
-        const username = elems[IDs.createUsername].value;
-        const request = backend.registerUser(username);
-        requestFeedback(request, elems[IDs.createUserButton], "", "Error");
-        request.then(response => {
-            alert(response);
-            redirectTo("app", username);
-        });
+    //redirect to sign_in page
+    elems[IDs.signinButton].addEventListener("click", _ => {
+            redirectTo("./sign_in");
     });
 
+
+    //deleting user. Not for this part! goes on app page
     elems[IDs.deleteUserButton].addEventListener("click", _ => {
         const username = elems[IDs.deleteUsername].value;
         const request = backend.deleteUser(username);
